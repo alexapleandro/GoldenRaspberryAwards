a4baf12b7464e5ef96693d710e63f8e7c1303946
alex <alex@ingadigital.com.br>
2022-03-20 12:39:36 -0300

    Criação do componente de estudios vencedores

--- golden-raspberry-awards-app/src/app/dashboard/components/studio/studio-dashboard.component.spec.ts
+++ golden-raspberry-awards-app/src/app/dashboard/components/studio/studio-dashboard.component.spec.ts
@@ -1,25 +1,47 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Studio } from 'src/app/shared/interfaces/studio';
+import { Studios } from 'src/app/shared/interfaces/studios';
+
+import { DashboardModule } from '../../dashboard.module';
 
 import { StudioDashboardComponent } from './studio-dashboard.component';
 
-describe('StudioDashboardComponent', () => {
-  let component: StudioDashboardComponent;
-  let fixture: ComponentFixture<StudioDashboardComponent>;
+describe(StudioDashboardComponent.name, () => {
+	let component: StudioDashboardComponent;
+	let fixture: ComponentFixture<StudioDashboardComponent>;
+	let values: Studios = {
+		studios: [
+			{ name: '1', winCount: 1 },
+			{ name: '2', winCount: 2 },
+			{ name: '3', winCount: 3 },
+			{ name: '4', winCount: 4 },
+		]
+	}
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ StudioDashboardComponent ]
-    })
-    .compileComponents();
-  });
+	beforeEach(async () => {
+		await TestBed.configureTestingModule({
+			imports: [DashboardModule]
+		}).compileComponents();
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(StudioDashboardComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+		fixture = TestBed.createComponent(StudioDashboardComponent);
+		component = fixture.componentInstance;
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+		fixture.detectChanges();
+	});
+
+	it('should create', () => {
+		expect(component).toBeTruthy();
+	});
+
+	it('should return from function filterValue', () => {
+
+		let value = component.filterValue(values);
+		expect(value.studios.length).toBe(3);
+	});
+
+	it('should return from function filterValue', () => {
+		let value = component.sortDesc(values.studios);
+		let data = value[0];
+		expect(data.winCount).toBe(4);
+	});
 });
--- golden-raspberry-awards-app/src/app/dashboard/components/studio/studio-dashboard.component.ts
+++ golden-raspberry-awards-app/src/app/dashboard/components/studio/studio-dashboard.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, SimpleChanges, OnChanges } from '@angular/core';
+import { map, Observable } from 'rxjs';
+import { Studios } from 'src/app/shared/interfaces/studios';
+import { Studio } from 'src/app/shared/interfaces/studio';
 
 @Component({
-  selector: 'app-studio-dashboard',
-  templateUrl: './studio-dashboard.component.html',
-  styleUrls: ['./studio-dashboard.component.scss']
+	selector: 'app-studio-dashboard',
+	templateUrl: './studio-dashboard.component.html',
+	styleUrls: ['./studio-dashboard.component.scss']
 })
-export class StudioDashboardComponent implements OnInit {
+export class StudioDashboardComponent implements OnInit, OnChanges {
 
-  constructor() { }
+	@Input() public value = new Observable<Studios>();
 
-  ngOnInit(): void {
-  }
+	constructor() { }
+
+	ngOnInit(): void { }
+
+	ngOnChanges(changes: SimpleChanges): void {
+		if(changes['value']) {
+			this.value =this.value.pipe(
+				map((data: any) => this.filterValue(data))
+			);
+		}
+	}
+
+	filterValue(value: Studios): Studios{
+		value.studios = this.sortDesc(value.studios).slice(0, 3);
+		return value;
+	}
+
+	sortDesc(list: Studio[]){
+		return list.sort((a, b) => (a.winCount > b.winCount ? -1 : 1));
+	}
 
 }
