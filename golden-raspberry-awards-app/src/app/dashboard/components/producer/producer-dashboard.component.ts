247954d859aa3e59ebf409141e9fa8f80cc3eaf1
alex <alex@ingadigital.com.br>
2022-03-20 12:29:56 -0300

    Criação do componente Listar vencedores por ano

--- golden-raspberry-awards-app/src/app/dashboard/components/producer/producer-dashboard.component.ts
+++ golden-raspberry-awards-app/src/app/dashboard/components/producer/producer-dashboard.component.ts
@@ -1,18 +1,15 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { Observable } from 'rxjs';
-import { ProducerMinMax } from 'src/app/shared/interfaces/producer-min-max';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
-	selector: 'app-producer-dashboard',
-	templateUrl: './producer-dashboard.component.html',
-	styleUrls: ['./producer-dashboard.component.scss']
+  selector: 'app-producer-dashboard',
+  templateUrl: './producer-dashboard.component.html',
+  styleUrls: ['./producer-dashboard.component.scss']
 })
 export class ProducerDashboardComponent implements OnInit {
 
-	@Input() public value = new Observable<ProducerMinMax>();
+  constructor() { }
 
-	constructor() { }
-
-	ngOnInit(): void { }
+  ngOnInit(): void {
+  }
 
 }
